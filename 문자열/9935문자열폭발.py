0f72fe6836676e8bbef919d471cc1ce039855622
sol20s <sol20s@naver.com>
2023-04-27 17:55:46 +0900

    김태형

---
+++ 문자열/9935문자열폭발.py
@@ -0,0 +1,13 @@
+st = input()
+target = input()
+n, m = len(st), len(target)
+stack = []
+remv = 0
+for i in range(n):
+    t
+    for j in range(m):
+        if st[i+j] == target[j]: 
+            continue
+        else:
+            break
+    stack.append(st)
